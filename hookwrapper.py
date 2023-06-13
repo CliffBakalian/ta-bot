d1a19bbb8a556a3b780fa48fae7afba8d2a71797
cliff <cliffbakalian@gmail.com>
2023-06-12 22:58:07 -0400

    removed day check. Will let cron handle that

--- hookwrapper.py
+++ hookwrapper.py
@@ -12,13 +12,11 @@ config = dotenv_values(dirs['TABOTENV'])
 tas = sys.argv[1]
 if tas == 'meeting':
   guild = sys.argv[2]
-  day = datetime.now().weekday()
-  if config[guild+"_DAY"] == str(day):
-    message = "We have a TA meeting today at "+config[guild+"_TIME"]
-    if config[guild+"_PLACE"]:
-      message += " in " + config[guild+"_PLACE"]
-    who = "everyone"
-    task(guild, who, message)
+  message = "We have a TA meeting today at "+config[guild+"_TIME"]
+  if config[guild+"_PLACE"]:
+    message += " in " + config[guild+"_PLACE"]
+  who = "everyone"
+  task(guild, who, message)
 elif tas == 'timesheets':
   task(sys.argv[2],ugrads,"Don't forget about timesheets")
 elif tas == 'regrades':
